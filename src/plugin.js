ed524fbaa940651fd56cf01e79d7121fd7001f9e
Craig Spence <craigspence0@gmail.com>
2015-11-30 18:19:33 +1300

    Add options from config

--- src/plugin.js
+++ src/plugin.js
@@ -7,9 +7,8 @@ import { translate as translateTS } from 'plugin-typescript';
 export function translate (load) {
 	return translateTS(load)
 	.then((load) => {
-		let annotate = ngAnnotate(load.source, {
-			add: true,
-			remove: true
+		let annotate = ngAnnotate(load.source, System.ngAnnotateOptions || {
+			add: true
 		});
 		load.source = annotate.src;
 		return load;
